d837d8197493ba5e4032e639958877feb284fd42
brettbittola <brettbittola@gmail.com>
2024-08-11 22:56:14 -0500

    Update db-connector.js

--- database/db-connector.js
+++ database/db-connector.js
@@ -10,9 +10,9 @@ var mysql = require('mysql')
 var pool = mysql.createPool({
     connectionLimit : 10,
     host            : 'classmysql.engr.oregonstate.edu',
-    user            : 'cs340_mccaffrl',
-    password        : '4699',
-    database        : 'cs340_mccaffrl'
+    user            : 'cs340_username',
+    password        : '****',
+    database        : 'cs340_username'
 })
 
 // Export it for use in our applicaiton
